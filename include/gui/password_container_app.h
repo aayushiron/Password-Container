148984cacfd9857b71b011e8c5007e859f979481
Aayush Bhat <aayushbhat2@gmail.com>
2020-11-14 15:22:13 -0800

    Add ImGui CinderBlock and create simple app with it

--- include/gui/password_container_app.h
+++ include/gui/password_container_app.h
@@ -4,6 +4,7 @@
 #include <cinder/app/App.h>
 #include <cinder/app/RendererGl.h>
 #include <cinder/gl/gl.h>
+#include <CinderImGui.h>
 
 namespace passwordcontainer {
 
@@ -13,16 +14,23 @@ class PasswordContainerApp : public ci::app::App {
  public:
   PasswordContainerApp();
 
+  void setup() override;
   void draw() override;
+  void update() override;
 
  private:
+  // Boolean to check if the button has been pressed
+  bool button_pressed_;
+
+  // The number of times the button has been pressed
+  size_t counter_;
+
   // Constants for the window size
-  const double kWindowSize = 875;
-  const double kMargin = 100;
+  const double kWindowSize = 300;
 };
 
-}
+}  // namespace gui
 
-}
+}  // namespace passwordcontainer
 
 #endif //GUI_PASSWORD_CONTAINER_MAIN_H
