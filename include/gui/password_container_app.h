33866d72c30a9b4bd9e6215ae65a6406451fa01f
Aayush Bhat <aayushbhat2@gmail.com>
2020-12-02 01:33:33 -0800

    Add the Account List to the app

--- include/gui/password_container_app.h
+++ include/gui/password_container_app.h
@@ -5,6 +5,10 @@
 #include <cinder/app/App.h>
 #include <cinder/app/RendererGl.h>
 #include <cinder/gl/gl.h>
+#include <string>
+
+#include "core/password_container.h"
+#include "gui/account_list.h"
 
 namespace passwordcontainer {
 
@@ -14,19 +18,32 @@ class PasswordContainerApp : public ci::app::App {
  public:
   PasswordContainerApp();
 
+  // Overridden Cinder methods to setup the app, draw the window, and updating
+  // values.
   void setup() override;
   void draw() override;
   void update() override;
 
  private:
-  // Boolean to check if the button has been pressed
-  bool button_pressed_;
+  // The container used to store account information
+  PasswordContainer container_;
 
-  // The number of times the button has been pressed
-  size_t counter_;
+  // The account list object used as the main window
+  AccountList account_list_;
+
+  // Booleans to track the action the user expects to be executed
+  bool is_modification_requested_;
+  bool is_addition_requested_;
+  bool is_deletion_requested_;
+  bool is_container_loaded_;
+  bool is_key_change_requested_;
+
+  // The index of the selected item in the list of accounts. Defaults to a value
+  // where there is no account selected.
+  int selected_item_ = kNoAccountSelectedIndex;
 
   // Constants for the window size
-  const double kWindowSize = 300;
+  const double kWindowSize = 600;
 };
 
 }  // namespace gui
--- src/gui/password_container_app.cc
+++ src/gui/password_container_app.cc
@@ -1,15 +1,25 @@
 #include "gui/password_container_app.h"
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "core/util.h"
 
 namespace passwordcontainer {
 
 namespace gui {
 
-PasswordContainerApp::PasswordContainerApp() {
+PasswordContainerApp::PasswordContainerApp() : container_(100, "CorrectKey"),
+      account_list_(container_, is_modification_requested_,
+                    is_deletion_requested_, is_addition_requested_,
+                    is_key_change_requested_, selected_item_) {
   ci::app::setWindowSize((int)kWindowSize, (int)kWindowSize);
-  button_pressed_ = false;
-  counter_ = 0;
+
+  // Loads in the data for the container from the specified file.
+  // TODO: CREATE WAY FOR USER TO SPECIFY FILE TO BE LOADED IN
+  std::ifstream file("../../../tests/resources/Data.pwords");
+  file >> container_;
 }
 
 void PasswordContainerApp::setup() {
@@ -17,24 +27,15 @@ void PasswordContainerApp::setup() {
 }
 
 void PasswordContainerApp::draw() {
-  bool my_tool_active = true;
-  ui::Begin("Counter App", &my_tool_active, ImGuiWindowFlags_MenuBar);
-
   // Clears the background of the cinder app
   ci::Color8u background_color("black");
   ci::gl::clear(background_color);
 
-  // Adds the counter text and button
-  std::string counter_text = "Counter: " + std::to_string(counter_);
-  ui::Text("%s", counter_text.c_str());
-  button_pressed_ = ui::Button("Add 1");
-  ui::End();
+  account_list_.DrawWindowAndUpdateValues();
 }
 
 void PasswordContainerApp::update() {
-  if (button_pressed_) {
-    counter_++;
-  }
+
 }
 
 }  // namespace gui
